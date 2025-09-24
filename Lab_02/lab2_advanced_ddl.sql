12ed863ab8295ec2f3699acfc102e387fd243582
Nurkhan <kyrykbay6@gmail.com>
2025-09-24 14:54:32 +0500

    1 sum

--- Lab_02/lab2_advanced_ddl.sql
+++ Lab_02/lab2_advanced_ddl.sql
@@ -3,36 +3,37 @@
 -- Part - 1
 -- Task - 1.1
 
-CREATE DATEBASE university_main
+CREATE DATABASE university_main
     WITH OWNER = postgres
     TEMPLATE = template0
     ENCODING = 'UTF8';
 
-CREATE DATEBASE university_archive
+CREATE DATABASE university_archive
     CONNECTION LIMIT = 50
     TEMPLATE = template0;
 
-CREATE DATEBASE university_test
-    IS_TAMPLATE = True
+CREATE DATABASE university_test
+    IS_TEMPLATE = True
     CONNECTION LIMIT = 10
     TEMPLATE = template0;
 
--- Part - 2
--- Task - 2.1
+-- Task - 1.2
 
 CREATE TABLESPACE student_data
     LOCATION 'C:/data/students';
 
 CREATE TABLESPACE course_data
-    LOCATION 'C:/data/students';
+    LOCATION 'C:/data/courses';
 
-CREATE DATEBASE university_distributed
+CREATE DATABASE university_distributed
     TABLESPACE = student_data
     ENCODING = 'LATIN9'
     CONNECTION LIMIT = -1;
 
+-- Part - 2
+-- Task - 2.1
 
-
+-- 1 
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -44,10 +45,11 @@ CREATE TABLE students (
     gpa NUMERIC(3,2) CHECK (gpa >= 0 AND gpa <= 4.0)
 );
 
+-- 2
 CREATE TABLE professors(
     professor_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
-    last_name VAECHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     office_number VARCHAR(20),
     hire_date DATE NOT NULL,
@@ -56,6 +58,7 @@ CREATE TABLE professors(
     years_of_experience INT CHECK (years_of_experience >= 0)
 );
 
+-- 3
 CREATE TABLE courses(
     course_id SERIAL PRIMARY KEY,
     course_code CHAR(8) UNIQUE NOT NULL,
@@ -65,14 +68,14 @@ CREATE TABLE courses(
     max_enrollment INT CHECK (max_enrollment > 0),
     course_fee NUMERIC(10, 2) CHECK (course_fee >= 0),
     is_online BOOLEAN DEFAULT TRUE,
-    created_at TIMESTMAP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Task - 2.2
-
+-- 1
 CREATE TABLE class_schedule(
     schedule_id SERIAL PRIMARY KEY,
-    course_id INT NOT NULL REFERENCES course(sourse_id) ON DELETE CASCADE,
+    course_id INT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
     professor_id INT NOT NULL REFERENCES professors(professor_id) ON DELETE SET NULL,
     classroom VARCHAR(20),
     class_date DATE NOT NULL,
@@ -81,25 +84,27 @@ CREATE TABLE class_schedule(
     duration INTERVAL,
     CHECK (end_time > start_time)
 );
+
+-- 2
 CREATE TABLE student_records(
     record_id SERIAL PRIMARY KEY,
     student_id INT NOT NULL REFERENCES students(student_id) ON DELETE CASCADE,
     course_id INT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
     semester VARCHAR(20) NOT NULL,
     year INT CHECK (year >= 2000),
-    greade CHAR(2),
+    grade CHAR(2),
     attendance_percentage NUMERIC(4, 2) CHECK (attendance_percentage >= 0 AND attendance_percentage <=100),
-    submission_timestamp TIMESTMAP NOT NULL DEFAULT NOW(),
-    last_updated TIMESTMAP NOT NULL DEFAULT NOW()
+    submission_timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    last_updated TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Part - 3
 -- Task - 3.1
 
 ALTER TABLE students
-    ADD COLUMN middle_name VERCHAR(30),
+    ADD COLUMN middle_name VARCHAR(30),
     ADD COLUMN student_status VARCHAR(20),
-    ALTER COLUMN phone TYPE VERCHAR(20),
+    ALTER COLUMN phone TYPE VARCHAR(20),
     ALTER COLUMN student_status SET DEFAULT 'active',
     ALTER COLUMN gpa SET DEFAULT 0.00;
 
@@ -108,20 +113,22 @@ ALTER TABLE professors
     ADD COLUMN research_area TEXT,
     ALTER COLUMN years_of_experience TYPE SMALLINT,
     ALTER COLUMN is_tenured SET DEFAULT FALSE,
-    ALTER COLUMN last_promotion_date TYPE DATE;
+    ADD COLUMN last_promotion_date DATE;
 
 ALTER TABLE courses
-    ADD COLUMN prerequisite_course_id INT REFERENCES courses(sourse_id) ON DELETE SET NULL,
-    ALTER COLUMN difficulty_level TYPE SMALLINT,
+    ADD COLUMN prerequisite_course_id INT REFERENCES courses(course_id) ON DELETE SET NULL,
+    ADD COLUMN difficulty_level INT,
     ALTER COLUMN course_code TYPE VARCHAR(10),
-    ALTER TABLE credits SET DEFAULT 3;
+    ALTER COLUMN credits SET DEFAULT 3,
     ADD COLUMN lab_required BOOLEAN DEFAULT FALSE;
 
 ALTER TABLE class_schedule
     ADD COLUMN room_capacity INT CHECK (room_capacity > 0),
     DROP COLUMN duration,
-    ALTER COLUMN course_code TYPE VERCHAR(10),
-    ALTER COLUMN credits SET DEFAULT 3;
+    ADD COLUMN course_code VARCHAR(5),
+    ALTER COLUMN course_code TYPE VARCHAR(10),
+    ADD COLUMN credits VARCHAR(10),
+    ALTER COLUMN credits SET DEFAULT 3,
     ADD COLUMN lab_required BOOLEAN DEFAULT FALSE;
 
 -- Task - 3.2
@@ -136,7 +143,7 @@ ALTER TABLE student_records
 -- Part - 4
 -- Task - 4.1
 
-CREATE COLUMN departments(
+CREATE TABLE departments(
     department_id SERIAL PRIMARY KEY,
     department_name VARCHAR(100) NOT NULL,
     department_code CHAR(5) UNIQUE NOT NULL,
@@ -211,7 +218,7 @@ CREATE TABLE grade_scale(
     max_percentage NUMERIC(4, 1) CHECK (max_percentage >= 0 AND max_percentage <= 100),
     gpa_points NUMERIC(3, 2) CHECK (gpa_points >= 0 AND gpa_points <= 4.0),
     description TEXT
-)
+);
 
 DROP TABLE IF EXISTS semester_calendar CASCADE;
 
@@ -227,14 +234,9 @@ CREATE TABLE semester_calendar(
 
 -- Task - 5.2
 
-DROP DATEBASE IF EXISTS university_test;
-DROP DATEBASE IF EXISTS university_distributed;
+DROP DATABASE IF EXISTS university_test;
 
-CREATE DATEBASE university_data
-    WITH OWNER = postgres
-    TABLESPACE = student_data
-    ENCODING = 'LATIN9'
-    CONNECTION LIMIT = -1;
+DROP DATABASE IF EXISTS university_distributed;
 
-CREATE TABLESPACE faculty_data
-    LOCATION 'C:/data/faculty';
+CREATE DATABASE university_backup
+    WITH TEMPLATE = university_main;
